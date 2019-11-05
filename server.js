99f225664daeab699e0280e51e1bbad743bb05f9
drmeloy <drmeloy@gmail.com>
2019-11-05 10:40:03 -0700

    added weather route

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 const geoData = require('./data/geo.json');
+const darkSky = require('./data/darksky.json');
 
 const getLatLng = (location) => {
     if (location === 'bad location'){
@@ -16,6 +17,14 @@ const getLatLng = (location) => {
     }
 
     return toLocation(geoData);
+};
+
+const getLatLngWeather = (location) => {
+    if (location === 'bad location'){
+        throw new Error();
+    }
+
+    return toLocation(darkSky);
 };
 
 const toLocation = (geoData) => {
@@ -35,6 +44,17 @@ app.get('/location', (request, response) => {
     try {
         const location = request.query.location;
         const result = getLatLng(location);
+        response.status(200).json(result);
+    }
+    catch (err){
+        response.status(500).send('Sorry, something dun fked');
+    }
+});
+
+app.get('/weather', (request, response) => {
+    try {
+        const location = request.query.weather;
+        const result = getLatLngWeather(location);
         response.status(200).json(result);
     }
     catch (err){
